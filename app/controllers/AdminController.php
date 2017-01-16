4d4275701a32e95ac6d0853b90139df3b2f406ba
Rudy Halim <rudyhalim.microsoft@gmail.com>
2017-01-16 14:55:32 +0700

    add back cache folders stable

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace admin;
+
+class AdminController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+    	echo "hai";
+    }
+
+}
+
---
+++ app/controllers/AutoreportingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Autoreporting;
+
+class AutoreportingController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
